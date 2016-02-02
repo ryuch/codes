406f422c4a1dd2a92a7aacea857d153baa0d656f
RYU Cheol <ryuch@yahoo.com>
2016-02-02 10:01:39 +0900

    specify range

--- nmuseum-download.py
+++ nmuseum-download.py
@@ -1,19 +1,28 @@
+#!/usr/bin/python
+
 import os
 import subprocess
+import sys
 
+if len(sys.argv) != 3:
+      sys.exit("two numbers required")
 
-for i in range(368, 10000):
-#   out= subprocess.check_output(["/usr/local/bin/wget", "-qO-", "\"http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+"\""])
-      out= subprocess.check_output(["/usr/local/bin/wget", "-qO-", "http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+""])
+for i in range(int(sys.argv[1]), int(sys.argv[2])):
+      out= subprocess.check_output(["/usr/bin/wget", "-qO-", "http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+""])
       pos1=out.find("<h4>")
       if(pos1>-1):
             pos2=out.find("</h4>",pos1)
             print i, out[pos1+4:pos2],
 
-            out= subprocess.check_output(["/usr/local/bin/wget", "--content-disposition", "http://www.museum.go.kr/site/main/relic/download/file/get?relicId="+str(i)+
-                                          "&imgPathNo=1"], stderr=subprocess.STDOUT)
+            out= subprocess.check_output(["/usr/bin/wget", "--content-disposition",
+                                          "http://www.museum.go.kr/site/main/relic/download/file/get?relicId="+str(i)+"&imgpathno=1"],
+                                         stderr=subprocess.STDOUT)
+            print out
 
             pos1=out.find("Saving to: '")
             if(pos1>-1):
                   pos2=out.find("\n", pos1+2)
-                  print out[pos1+12:pos2-1]
+                  print out[pos1+12:pos2-1],
+      print ""
+
+sys.exit("done")
