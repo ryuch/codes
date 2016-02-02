191b99ccf0c8dfd8f3be3ed589e965992058c500
RYU Cheol <rcheol@gmail.com>
2016-02-02 22:31:16 +0900

    sleep 3

--- nmuseum-download.py
+++ nmuseum-download.py
@@ -5,11 +5,15 @@ import subprocess
 import sys
 import time
 
+#wget_cmd = "/usr/bin/wget"
+wget_cmd = "/usr/local/bin/wget"
+
+
 if len(sys.argv) != 3:
         sys.exit("two numbers required")
 
 for i in range(int(sys.argv[1]), int(sys.argv[2])):
-        out= subprocess.check_output(["/usr/bin/wget", "-qO-", "http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+""])
+        out= subprocess.check_output([wget_cmd, "-qO-", "http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+""])
         if out < 0 : continue
         pos1=out.find("<h4>")
         if(pos1>-1):
@@ -17,7 +21,7 @@ for i in range(int(sys.argv[1]), int(sys.argv[2])):
                 print i, out[pos1+4:pos2],
 
                 # ex) http://www.museum.go.kr/site/main/relic/download/file/get?relicId=329&imgPathNo=1
-                out= subprocess.check_output(["/usr/bin/wget", "--content-disposition",
+                out= subprocess.check_output([wget_cmd, "--content-disposition",
                                                                                           "http://www.museum.go.kr/site/main/relic/download/file/get?relicId="+str(i)+"&imgPathNo=1"],
                                                                                          stderr=subprocess.STDOUT)
                 if out < 0 : continue
@@ -25,10 +29,10 @@ for i in range(int(sys.argv[1]), int(sys.argv[2])):
                 pos1=out.find("Saving to: ")
                 if(pos1>-1):
                         pos2=out.find("\n", pos1+2)
-                        print out[pos1+14:pos2-3],
+                        print out[pos1+12:pos2-1],
 
                 print ""
                 sys.stdout.flush()
-		time.sleep(10)
+		time.sleep(3)
 
 sys.exit("done")
