3cf1f3f681f9cf2ab07c6aab721a5ddeca6bdbc0
RYU Cheol <ryuch@yahoo.com>
2016-02-02 19:50:59 +0900

    strange test

---
+++ nmuseum-download.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import os
+import subprocess
+import sys
+
+if len(sys.argv) != 3:
+        sys.exit("two numbers required")
+
+for i in range(int(sys.argv[1]), int(sys.argv[2])):
+        out= subprocess.check_output(["/usr/bin/wget", "-qO-", "http://www.museum.go.kr/site/main/relic/search/view?relicId="+str(i)+""])
+        if out < 0 : continue
+        pos1=out.find("<h4>")
+        if(pos1>-1):
+                pos2=out.find("</h4>",pos1)
+                print i, out[pos1+4:pos2],
+
+                # ex) http://www.museum.go.kr/site/main/relic/download/file/get?relicId=329&imgPathNo=1
+                out= subprocess.check_output(["/usr/bin/wget", "--content-disposition",
+                                                                                          "http://www.museum.go.kr/site/main/relic/download/file/get?relicId="+str(i)+"&imgPathNo=1"],
+                                                                                         stderr=subprocess.STDOUT)
+                if out < 0 : continue
+
+                pos1=out.find("Saving to: ")
+                if(pos1>-1):
+                        pos2=out.find("\n", pos1+2)
+                        print out[pos1+14:pos2-3],
+
+                print ""
+                sys.stdout.flush()
+
+sys.exit("done")
